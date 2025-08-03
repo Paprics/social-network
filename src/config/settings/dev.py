0163644677ebfb9931ab6d2317ad33ccb82b98ec
Valeriy D. <paprics@gmail.com>
2025-08-03 15:31:59 +0300

    Integrate Postgres DB into Docker (dev branch)

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -48,17 +48,69 @@ else:
     }
 
 # message broker для Django Channels
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels.layers.InMemoryChannelLayer",
-#     }
-# }
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [(os.getenv("REDIS_HOST", "localhost"), int(os.getenv("REDIS_PORT", 6379)))],
+if os.environ.get("MODE_CHANNEL_LAYERS") == '0':
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+        }
+    }
+elif os.environ.get("MODE_CHANNEL_LAYERS") == '1':
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [(os.getenv("REDIS_HOST", "localhost"), int(os.getenv("REDIS_PORT", 6379)))],
+            },
         },
-    },
-}
+    }
+else:
+    raise RuntimeError("MODE_CHANNEL_LAYERS must be 0 (InMemory) or 1 (Redis)")
+
+# DATABASES
+if os.environ.get("GITHUB_WORKFLOW"):
+    # GitHub Actions → PostgreSQL (CI)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "0.0.0.0",
+            "PORT": 5432,
+        }
+    }
+
+elif os.environ.get("DOCKER_ENV") == "1":
+    # Docker → PostgreSQL (берёт переменные из .env)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST", "postgres_db"),
+            "PORT": os.environ.get("POSTGRES_PORT", 5432),
+        }
+    }
+elif os.environ.get("MODE_DATABASE") == "0":
+    # По умолчанию SQLite (чисто для разработки без всего)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+elif os.environ.get("MODE_DATABASE") == "1":
+    # Локальная PostgreSQL
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        }
+    }
+else:
+    raise RuntimeError("MODE_DATABASE is not set correctly (expected 0 or 1)")
